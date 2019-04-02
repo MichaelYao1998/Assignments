89a0f34bbea8c8c942d1dbd2ad191f0af4416ef6
MichaelYao1998 <32200218+MichaelYao1998@users.noreply.github.com>
2019-04-02 21:18:07 +1100

    update

--- HeapFile/src/WriteFile.java
+++ HeapFile/src/WriteFile.java
@@ -3,7 +3,10 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 public class WriteFile {
-
+    /*
+      read the csv file and then use DataOutputStream to write it into a binary file.
+       And it will also calculate the pages it need and numbers of records it loaded.
+     */
     public void WriteToFile(String fileName, String HeapFileName, int pageSize) {
         byte[] data = new byte[300];
         int PageNumber, dataNumber;
@@ -61,24 +64,52 @@ public class WriteFile {
         System.arraycopy(content, 0, data, dataOffset, content.length);
     }
 
-        //creating dataset/record and apply the offset when necessary
-    public byte[] dataset(byte[] data, String[] words, int extraPage) {
+    /*creating dataset/record and apply the offset when necessary.
+       dataoffset = the size of previous records
+    */
+    private byte[] dataset(byte[] data, String[] words, int extraPage) {
         byte[] dID = intToByteArr(extraPage);
-        System.arraycopy(dID,0,data,0,dID.length);
-        createByteArr(words[0],40,15,data);
-        createByteArr(words[1],15,55,data);
-        createByteArr(words[2],30,70,data);
-        createByteArr(words[3],30,100,data);
-        createByteArr(words[4],15,130,data);
-        createByteArr(words[5],20,145,data);
-        createByteArr(words[6],40,165,data);
-        createByteArr(words[7],20,205,data);
-        createByteArr(words[8],15,225,data);
-        createByteArr(words[9],30,240,data);
-        createByteArr(words[10],30,270,data);
-        createByteArr(words[11],30,270,data);
-        createByteArr(words[12],15,271,data);
-        createByteArr(words[13],15,272,data);
+        int DA_NAME_SIZE = 40;
+        int DEVICE_ID_SIZE = 15;
+        int ARRIVAL_TIME_SIZE = 30;
+        int DEPARTURE_TIME_SIZE = 30;
+        int DURATION_SECONDS_SIZE = 15;
+        int STREET_MARKER_SIZE = 20;
+        int SIGN_SIZE = 40;
+        int AREA_SIZE = 20;
+        int STREET_ID_SIZE = 15;
+        int STREET_NAME_SIZE = 30;
+        int BETWEEN_ST_1_SIZE =30;
+        int BETWEEN_ST_2_SIZE = 30;
+        int SIDE_OF_STREET_SIZE = 15;
+        int IN_VIOLATEION_SIZE = 15;
+
+        int DA_NAME_OFFSET = DA_NAME_SIZE;
+        int DEVICE_ID_OFFSET = DA_NAME_SIZE + DEVICE_ID_SIZE;
+        int ARRIVAL_TIME_OFFSET = DEVICE_ID_OFFSET + ARRIVAL_TIME_SIZE;
+        int DEPARTURE_TIME_OFFSET = ARRIVAL_TIME_OFFSET + DEPARTURE_TIME_SIZE;
+        int DURATION_SECONDS_OFFSET = DEPARTURE_TIME_OFFSET + DURATION_SECONDS_SIZE;
+        int STREET_MARKER_OFFSET = DURATION_SECONDS_OFFSET + STREET_MARKER_SIZE;
+        int SIGN_OFFSET = STREET_MARKER_OFFSET + SIGN_SIZE;
+        int AREA_OFFSET = SIGN_OFFSET + AREA_SIZE;
+        int STREET_ID_OFFSET = AREA_OFFSET + STREET_ID_SIZE;
+        int STREET_NAME_OFFSET = STREET_ID_OFFSET + STREET_NAME_SIZE;
+
+        System.arraycopy(dID, 0, data, 0, dID.length);
+        createByteArr(words[0], DA_NAME_SIZE, DA_NAME_OFFSET, data);
+        createByteArr(words[1], DEVICE_ID_SIZE, DEVICE_ID_OFFSET, data);
+        createByteArr(words[2], ARRIVAL_TIME_SIZE, ARRIVAL_TIME_OFFSET, data);
+        createByteArr(words[3], DEPARTURE_TIME_SIZE, DEPARTURE_TIME_OFFSET, data);
+        createByteArr(words[4], DURATION_SECONDS_SIZE, DURATION_SECONDS_OFFSET, data);
+        createByteArr(words[5], STREET_MARKER_SIZE , STREET_MARKER_OFFSET, data);
+        createByteArr(words[6], SIGN_SIZE , SIGN_OFFSET, data);
+        createByteArr(words[7], AREA_SIZE, AREA_OFFSET, data);
+        createByteArr(words[8], STREET_ID_SIZE, STREET_ID_OFFSET, data);
+        createByteArr(words[9], STREET_NAME_SIZE , STREET_NAME_OFFSET, data);
+        createByteArr(words[10], BETWEEN_ST_1_SIZE , 269, data);
+        createByteArr(words[11], BETWEEN_ST_2_SIZE, 270, data);
+        createByteArr(words[12], SIDE_OF_STREET_SIZE, 271, data);
+        createByteArr(words[13], IN_VIOLATEION_SIZE, 272, data);
         return data;
     }
 
